8c9b1d1507a201fbcf547704f9bdb0f3606e22a3
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-04 09:11:15 -0300

    change config to deploy

--- src/templates/main.js
+++ src/templates/main.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
 const middy = require('@middy/core')
 const jsonBodyParser = require('@middy/http-json-body-parser')
+const httpMultipartBodyParser = require('@middy/http-multipart-body-parser')
 const { preflight } = require('@ziro/middleware')
+const { allowedOrigin } = require('@ziro/middleware')
 const { auth } = require('@ziro/middleware')
 const { errorHandler } = require('@ziro/middleware')
 const { cors } = require('@ziro/middleware')
+const allowed = null // <----------- CHANGE TO NULL BEFORE DEPLOY
 
 const lambda = handler =>
-	middy(handler)
-	.use(preflight)
-	.use(auth)
-	.use(jsonBodyParser())
-	.use(errorHandler)
-	.use(cors)
+    middy(handler)
+        .use(preflight)
+        .use(allowedOrigin(allowed))
+        .use(auth)
+        .use(jsonBodyParser())
+        .use(httpMultipartBodyParser())
+        .use(errorHandler)
+        .use(cors)
 
 module.exports = lambda

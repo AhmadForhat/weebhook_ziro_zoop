c05b81ca46b66b8e2bb0a96068c49d7a8c0dac46
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-04 08:57:35 -0300

    resolvendo conflitos

--- src/templates/main.js
+++ src/templates/main.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const middy = require('@middy/core')
-const { basicAuth } = require('@ziro/middleware')
-const httpJsonBodyParser = require('@middy/http-json-body-parser')
-const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
-const cors = require('@middy/http-cors')
+const jsonBodyParser = require('@middy/http-json-body-parser')
+const { preflight } = require('@ziro/middleware')
+const { auth } = require('@ziro/middleware')
+const { errorHandler } = require('@ziro/middleware')
+const { cors } = require('@ziro/middleware')
 
-const main = handler =>
-    middy(handler)
-        .use(basicAuth)
-        .use(httpJsonBodyParser())
-        .use(httpErrorHandler())
-        .use(cors())
+const lambda = handler =>
+	middy(handler)
+	.use(preflight)
+	.use(auth)
+	.use(jsonBodyParser())
+	.use(errorHandler)
+	.use(cors)
 
-module.exports = main
+module.exports = lambda

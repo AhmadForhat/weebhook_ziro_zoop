b93285f19c6fd5b984000343cf3d595ff639030d
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-04 08:21:00 -0300

    options google sheets

---
+++ src/utils/postSheets.js
@@ -0,0 +1,27 @@
+const optionsPost = (body) =>{
+    const url = process.env.URL_SHEET
+    const auth = `Basic ${process.env.TOKEN_SHEET}`
+
+    return {
+        method: 'POST',
+        url:url,
+        headers: {
+            'Origin': 'https://ziro.app',
+            'Content-type': 'application/json',
+            'Authorization': auth
+        },
+        body : {
+            'apiResource': 'values',
+            'apiMethod': 'append',
+            'spreadsheetId': process.env.ID_SHEET,
+            'range': 'boletos gerados!A2',
+            'resource': {
+                'values': [body]
+            },
+            'valueInputOption': 'raw'
+        },
+        json: true
+    }
+}
+
+module.exports = optionsPost

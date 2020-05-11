736d72b54d7b7625513ebe808bfaccb47d0a26bc
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-11 12:51:07 -0300

    change middy

--- src/templates/main.js
+++ src/templates/main.js
@@ -3,16 +3,16 @@ const middy = require('@middy/core')
 const jsonBodyParser = require('@middy/http-json-body-parser')
 const httpMultipartBodyParser = require('@middy/http-multipart-body-parser')
 const { preflight } = require('@ziro/middleware')
-const { allowedOrigin } = require('@ziro/middleware')
+// const { allowedOrigin } = require('@ziro/middleware')
 // const { auth } = require('@ziro/middleware')
 const { errorHandler } = require('@ziro/middleware')
 const { cors } = require('@ziro/middleware')
-const allowed = null // <----------- CHANGE TO NULL BEFORE DEPLOY
+// const allowed = null // <----------- CHANGE TO NULL BEFORE DEPLOY
 
 const lambda = handler =>
     middy(handler)
         .use(preflight)
-        .use(allowedOrigin(allowed))
+        // .use(allowedOrigin(allowed))
         .use(jsonBodyParser())
         .use(httpMultipartBodyParser())
         .use(errorHandler)

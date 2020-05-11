d59072a36d44b3b77da82944fff988a0deed524f
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-11 12:51:21 -0300

    tirando firebase

--- src/lambdas/updateStatus.js
+++ src/lambdas/updateStatus.js
@@ -1,6 +1,6 @@
 const main = require('../templates/main')
 const rp = require('request-promise-native')
-const { db } = require('../firebase/firabaseConfig')
+// const { db } = require('../firebase/firabaseConfig')
 const postSheet = require('../utils/postSheets')
 const translateStatus = require('../utils/translateStatus')
 const { dateHourFormatterUTC3, dataHourFromatterZoop } = require('../utils/dateHourFormatterUTC3')
@@ -18,27 +18,27 @@ const getEmployees = async ({ body }) => {
         translateStatus(statusNovo)
     ]
     try {
-        console.log(body)
         const requestSheets = await rp(postSheet(corpo))
-        console.log('responsa do update no googleSheets', requestSheets._rp_options.body)
-        try {
-            const requestFirebase = await db.collection('credit-card-payments')
-                .doc(id)
-                .update({
-                    status: translateStatus(statusNovo)
-                })
-            console.log('reposta do Firebase', requestFirebase)
-            return {
-                statusCode:200,
-                body: JSON.stringify({
-                    mensagem: 'Status salvo com sucesso no Sheet e no Firebase',
-                    data: dateHourFormatterUTC3(new Date()),
-                    statusTransacao: body.payload.status
-                })
-            }
-        } catch (error) {
-            console.log('erro no firebase', error)
+        console.log('responsa do update no googleSheets', requestSheets)
+        // Firebase
+        // try {
+        //     const requestFirebase = await db.collection('credit-card-payments')
+        //         .doc(id)
+        //         .update({
+        //             status: translateStatus(statusNovo)
+        //         })
+        // console.log('reposta do Firebase', requestFirebase)
+        return {
+            statusCode:200,
+            body: JSON.stringify({
+                mensagem: 'Status salvo com sucesso no Sheet e no Firebase',
+                data: dateHourFormatterUTC3(new Date()),
+                statusTransacao: body.payload.status
+            })
         }
+        // } catch (error) {
+        //     console.log('erro no firebase', error)
+        // }
     } catch (error) {
         console.log('erro no sheets', error)
         return {

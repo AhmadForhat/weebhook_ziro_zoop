0f786aea2520bf2e36d764de421008a8f0eac974
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-12 17:41:39 -0300

    teste com condicionais

--- src/lambdas/updateStatus.js
+++ src/lambdas/updateStatus.js
@@ -1,53 +1,74 @@
 const main = require('../templates/main')
-// const rp = require('request-promise-native')
+const rp = require('request-promise-native')
 // const { db } = require('../firebase/firabaseConfig')
-// const postSheet = require('../utils/postSheets')
-// const translateStatus = require('../utils/translateStatus')
+const postSheet = require('../utils/postSheets')
+const translateStatus = require('../utils/translateStatus')
 const { dateHourFormatterUTC3, dataHourFromatterZoop } = require('../utils/dateHourFormatterUTC3')
 
 require('dotenv').config()
 
-const updateStatus = async ({ body }) => {
-    // const { payload } = body
-    // const { id, status:statusNovo , payment_method } = payload
-    // const { updated_at } = payment_method
-    // const corpo = [
-    //     id,
-    //     dateHourFormatterUTC3(new Date()),
-    //     dateHourFormatterUTC3(dataHourFromatterZoop(updated_at)),
-    //     translateStatus(statusNovo)
-    // ]
-    try {
+const getEmployees = async ({ body }) => {
+    if(body.payload){
         console.log(body)
-        //Google Sheets
-        // const requestSheets = await rp(postSheet(corpo))
-        // console.log('responsa do update no googleSheets', requestSheets)
-        // Firebase
-        // try {
-        //     const requestFirebase = await db.collection('credit-card-payments')
-        //         .doc(id)
-        //         .update({
-        //             status: translateStatus(statusNovo)
-        //         })
-        // console.log('reposta do Firebase', requestFirebase)
+        const { payload } = body
+        const { id, status:statusNovo , payment_method } = payload
+        const { updated_at } = payment_method
+        const corpo = [
+            id,
+            dateHourFormatterUTC3(new Date()),
+            dateHourFormatterUTC3(dataHourFromatterZoop(updated_at)),
+            translateStatus(statusNovo)
+        ]
+        try {
+            const requestSheets = await rp(postSheet(corpo))
+            console.log('responsa do update no googleSheets', requestSheets)
+            // Firebase
+            // try {
+            //     const requestFirebase = await db.collection('credit-card-payments')
+            //         .doc(id)
+            //         .update({
+            //             status: translateStatus(statusNovo)
+            //         })
+            // console.log('reposta do Firebase', requestFirebase)
+            return {
+                statusCode:200,
+                body: JSON.stringify({
+                    mensagem: 'Status salvo com sucesso no Sheet e no Firebase',
+                    data: dateHourFormatterUTC3(new Date()),
+                    statusTransacao: body.payload.status
+                })
+            }
+            // } catch (error) {
+            //     console.log('erro no firebase', error)
+            // }
+        } catch (error) {
+            console.log('erro no sheets', error)
+            return {
+                statusCode: 500,
+                body: JSON.stringify(error)
+            }
+        }
+    }if(body.type === 'ping'){
+        console.log('ping',body)
         return {
-            statusCode:200,
+            statusCode: 200,
             body: JSON.stringify({
-                statusCode: 200,
-                mensagem: 'Status salvo com sucesso no Sheet e no Firebase',
+                mensagem: 'Webhook cadastrado com sucessso!!',
+                data: dateHourFormatterUTC3(new Date()),
+                id: body.id
+            })
+        }
+    }else{
+        console.log('não reconheceu body', body)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                mensagem: 'Body não reconhecido!!',
                 data: dateHourFormatterUTC3(new Date())
             })
         }
-        // } catch (error) {
-        //     console.log('erro no firebase', error)
-        // }
-    } catch (error) {
-        console.log('erro no sheets', error)
-        return {
-            statusCode: 500,
-            body: JSON.stringify(error)
-        }
     }
+
 }
 
-module.exports = { handler: main(updateStatus) }
+module.exports = { handler: main(getEmployees) }
